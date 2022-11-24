2da3a4f7c9b6e8480b01af099566d829b572f0d8
Evan <973295056@qq.com>
2022-11-24 16:48:14 +0800

    1

--- pytorch_classification/grad_cam/main_cnn.py
+++ pytorch_classification/grad_cam/main_cnn.py
@@ -26,31 +26,39 @@ def main():
 
     data_transform = transforms.Compose([transforms.ToTensor(),
                                          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-    # load image
-    img_path = "both.png"
-    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
-    img = Image.open(img_path).convert('RGB')
-    img = np.array(img, dtype=np.uint8)
-    # img = center_crop_img(img, 224)
 
-    # [C, H, W]
-    img_tensor = data_transform(img)
-    # expand batch dimension
-    # [C, H, W] -> [N, C, H, W]
-    input_tensor = torch.unsqueeze(img_tensor, dim=0)
+    # test_imgs
+    test_path = "./test_img"
+    img_list = os.listdir(test_path)
+    for imgs in img_list:
+        # load image
+        img_path = os.path.join(test_path, imgs)
+        assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+        img = Image.open(img_path).convert('RGB')
+        img = np.array(img, dtype=np.uint8)
+        # img = center_crop_img(img, 224)
 
-    cam = GradCAM(model=model, target_layers=target_layers, use_cuda=False)
-    target_category = 281  # tabby, tabby cat
-    # target_category = 254  # pug, pug-dog
+        # [C, H, W]
+        img_tensor = data_transform(img)
+        # expand batch dimension
+        # [C, H, W] -> [N, C, H, W]
+        input_tensor = torch.unsqueeze(img_tensor, dim=0)
 
-    grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category)
+        cam = GradCAM(model=model, target_layers=target_layers, use_cuda=False)
+        target_category = None  # tabby, tabby cat
+        # target_category = 254  # pug, pug-dog
 
-    grayscale_cam = grayscale_cam[0, :]
-    visualization = show_cam_on_image(img.astype(dtype=np.float32) / 255.,
-                                      grayscale_cam,
-                                      use_rgb=True)
-    plt.imshow(visualization)
-    plt.show()
+        grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category)
+
+        grayscale_cam = grayscale_cam[0, :]
+        visualization = show_cam_on_image(img.astype(dtype=np.float32) / 255.,
+                                          grayscale_cam,
+                                          use_rgb=True)
+        plt.imshow(visualization)
+        # plt.show()
+        plt.ion()
+        plt.pause(3)
+        plt.close()
 
 
 if __name__ == '__main__':
